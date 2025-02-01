315a83a36c48125cd90ceeedd15d1dc016f8adf2
serphico <gomez.ecm@gmail.com>
2025-02-01 17:53:25 -0300

    Agregado funcionalidades de agregar nombre y validar entrada

--- app.js
+++ app.js
@@ -11,24 +11,4 @@ function agregarAmigo(){
     document.getElementById('amigo').value == '' ? alert('Digite un nombre Valido') : nombreAmigos.push(document.getElementById('amigo').value);
 
     listaDeAmigos(nombreAmigos);
-}   
-
-function listaDeAmigos(amigos){
-
-    //Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
-
-    let listaDeAmigos = document.getElementById('listaAmigos');
-
-    listaDeAmigos.innerHTML = `<li> ${amigos} </li>`;
-    
-    return;
-}
-
-//Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página.
-
-function sortearAmigo() {
-    let numeroAleatorio = Math.floor(Math.random()*nombreAmigos.length)
-
-    nombreAmigos.length <= 1 || nombreAmigos == undefined  ? alert('Introduzca más nombres') :listaDeAmigos(` El ganador/a es: ${nombreAmigos[numeroAleatorio]}`);
-    console.log(numeroAleatorio);
-}
+} 
